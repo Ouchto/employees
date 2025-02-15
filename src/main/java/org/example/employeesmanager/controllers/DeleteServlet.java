f991a84bb789c4877b7ea9680a72dcd3243ec3c0
Ouchto <mustapha.enaa@gmail.com>
2025-02-15 13:18:07 +0100

    la Servlet pour supprimer une personne

--- src/main/java/org/example/employeesmanager/controllers/DeleteServlet.java
+++ src/main/java/org/example/employeesmanager/controllers/DeleteServlet.java
@@ -1,4 +1,31 @@
 package org.example.employeesmanager.controllers;
 
-public class DeleteServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.employeesmanager.dao.PersonDAO;
+
+
+import java.io.IOException;
+
+@WebServlet("/deletePerson")
+public class DeleteServlet extends HttpServlet {
+    private PersonDAO personDAO;
+
+    @Override
+    public void init() {
+        personDAO = new PersonDAO();
+    }
+
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        personDAO.deletePerson(id);
+
+        response.sendRedirect("readPerson");
+    }
 }
